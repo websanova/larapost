f11e4996a8966662cd5145b70a584f4b5d563714
websanova <rob@websanova.com>
2016-01-08 16:02:20 +0700

    Clean out redirects.

--- app/Parser/Field/RedirectFrom.php
+++ app/Parser/Field/RedirectFrom.php
@@ -6,48 +6,71 @@ use Websanova\Larablog\Models\Post;
 
 class RedirectFrom
 {
-	public static function process($key, $val, $data)
-	{
+    public static function process($key, $val, $data)
+    {
         return $data;
-	}
+    }
 
-	public static function handle($key, $val, $post)
-	{
-		if ( ! is_array($val)) {
-			$val = [$val];
-		}
+    public static function handle($key, $val, $post)
+    {
+        if ( ! is_array($val)) {
+            $val = [$val];
+        }
 
-		foreach ($val as $redirect_from) {
-			$redirect = Post::where('slug', $redirect_from)->first();
+        $redirects = [];
 
-			if ($redirect && $redirect->type !== 'redirect') {
-				echo '***********************************************************************' . "\n";
-				echo '* WARNING: Slug exists: ' . $redirect->slug . "\n";
-				echo '***********************************************************************' . "\n";
-			}
+        foreach ($val as $redirect_from) {
+            $redirect = Post::where('slug', $redirect_from)->first();
 
-			$data = [
-				'slug' => $redirect_from,
-				'title' => '',
-				'body' => '',
-				'type' => 'redirect',
-				'meta' => json_encode([
-					'redirect_to' => $post->slug
-				])
-			];
-			if ($redirect) {
-				$redirect->fill($data);
+            if ($redirect && $redirect->type !== 'redirect') {
+                echo '***********************************************************************' . "\n";
+                echo '* WARNING: Slug exists: ' . $redirect->slug . "\n";
+                echo '***********************************************************************' . "\n";
+            }
 
-				if ($redirect->isDirty()) {
-					$redirect->save();
-					echo 'Update Redirect: ' . $redirect_from . "\n";
-				}
-			}
-			else {
-				$redirect = Post::create($data);
+            $data = [
+                'slug' => $redirect_from,
+                'title' => '',
+                'body' => '',
+                'type' => 'redirect',
+                'meta' => json_encode([
+                    'redirect_to' => $post->slug
+                ])
+            ];
 
-				echo 'New Redirect: ' . $redirect_from . "\n";
-			}
-		}
-	}
+            if ($redirect) {
+                $redirect->fill($data);
+
+                if ($redirect->isDirty()) {
+                    $redirect->save();
+                    echo 'Update Redirect: ' . $redirect_from . "\n";
+                }
+            }
+            else {
+                $redirect = Post::create($data);
+
+                echo 'New Redirect: ' . $redirect_from . "\n";
+            }
+
+            array_push($redirects, $redirect->slug);
+        }
+
+        self::delete($redirects, $post);
+    }
+
+    public static function delete($redirects, $post)
+    {
+        $posts = Post::whereNotIn('slug', $redirects)
+            ->where('type', 'redirect')
+            ->where('meta', 'LIKE', '%"redirect_to":"' . str_replace('/', "\\\/", $post->slug) . '"%')
+            ->get();
+
+        if ( ! $posts->isEmpty()) {
+            Post::whereIn('id', $posts->lists('id')->toArray())->delete();
+
+            foreach ($posts as $p) {
+                echo 'Removed Redirect: ' . $p->slug . "\n";
+            }
+        }
+    }
 }
