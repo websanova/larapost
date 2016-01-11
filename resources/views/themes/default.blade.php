8e1e58f3fd6ac4aafe095d54951f59fdb6a48667
websanova <rob@websanova.com>
2016-01-11 19:25:20 +0700

    Update sidebar width for lg bootstrap size.

--- resources/views/themes/default.blade.php
+++ resources/views/themes/default.blade.php
@@ -40,10 +40,10 @@
 
 <div id="container" class="container">
     <div class="row">
-        <div class="col-xs-12 col-sm-8 col-md-9 col-lg-10">
+        <div class="col-xs-12 col-sm-8 col-md-9 col-lg-9">
 			@include ($view)
 	    </div>
-	    <div class="col-xs-12 col-sm-4 col-md-3 col-lg-2">
+	    <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
 	    	<br/>
 		    @include ('larablog::layout.sidebar')
 	    </div>
