f49f29aac298e502c439500b8f2ded6a01d2f0df
websanova <rob@websanova.com>
2016-01-11 21:26:15 +0700

    Fix search button to submit.

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -2,7 +2,7 @@
 	<div class="input-group">
 		<input type="text" name="q" class="form-control" placeholder="Search"/>
 		<span class="input-group-btn">
-			<button class="btn btn-default" type="button">Go</button>
+			<button class="btn btn-default" type="submit">Go</button>
 		</span>
     </div>
 </form>
