d8bcfb0989d938e4c4b176c9b0cd86e4cdfb5b25
websanova <rob@websanova.com>
2016-01-15 19:52:30 +0700

    Update footer header for local/* environments.

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,5 +1,10 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+@if (false)
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+@else
+    <script src="/lib/jquery.1.11.1.min.js"></script>
+    <script src="/lib/bootstrap.3.3.5.min.js"></script>
+@endif
 
 @foreach(config('larablog.site_footers') as $site_footer)
     @include ($site_footer)
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -15,14 +15,19 @@
     
     @include ('larablog::layout.meta')
 
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" type="text/css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    @if (false)
+        <link type="text/css" rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" />
+        <link type="text/css" rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+          <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    @else
+        <link type="text/css" rel="stylesheet" href="/lib/bootstrap.3.3.5.min.css" />
+        <link type="text/css" rel="stylesheet" href="/lib/font-awesome.4.3.0.min.css">
+    @endif
 </head>
 <body>
 
