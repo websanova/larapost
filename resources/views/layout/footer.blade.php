048d291a0c8b0da7be799841217b0942204be015
websanova <rob@websanova.com>
2016-01-16 14:28:47 +0700

    Clean up css/js includes.

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,10 +1,11 @@
-@if (false)
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-@else
-    <script src="/lib/jquery.1.11.1.min.js"></script>
-    <script src="/lib/bootstrap.3.3.5.min.js"></script>
-@endif
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
 
 @foreach(config('larablog.site_footers') as $site_footer)
     @include ($site_footer)
