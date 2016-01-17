14946f33bce093eacdc2424fecab3ef0949360bd
websanova <rob@websanova.com>
2016-01-17 18:20:56 +0700

    Fix space and typo.

--- resources/views/layout/meta.blade.php
+++ resources/views/layout/meta.blade.php
@@ -1,4 +1,4 @@
-<title>{{ @$title ?: (@$post->title ?: config('larablog.meta.title')) }} </title>
+<title>{{ @$title ?: (@$post->title ?: config('larablog.meta.title')) }}</title>
 
 <meta name="keywords" content="{{ @$keywords ?: (@$post->meta->keywords ?: config('larablog.meta.keywords')) }}" />
 <meta name="description" content="{{ @$description ?: (@$post->meta->description ?: config('larablog.meta.description')) }}" />
@@ -8,5 +8,5 @@
 <meta property="og:url" content="{{ url('/') }}{{ @$slug ?: (@$post->slug ?: '/' . Request::path()) }}" />
 <meta property="og:description" content="{{ @$description ?: (@$post->meta->description ?: config('larablog.meta.description')) }}" />
 <meta property="og:locale" content="{{ config('app.locale') }}" />
-<meta property="og:site_name" content="{{ config('larablog.site_name') }}" />
+<meta property="og:site_name" content="{{ config('larablog.site.name') }}" />
 <meta property="og:image" content="{{ url('/') }}{{ @$img ?: (@$post->meta->img ?: config('larablog.meta.logo')) }}" />
