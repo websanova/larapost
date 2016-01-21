1ecc33b2ff972fcc55b64bc19d39c985d827c611
websanova <rob@websanova.com>
2016-01-21 17:20:58 +0700

    Remove comments.

--- resources/views/themes/master.blade.php
+++ resources/views/themes/master.blade.php
@@ -1,34 +1,3 @@
 @extends ('larablog::themes.' . config('larablog.app.theme') . '.layout')
 
 @include ('larablog::themes.overrides')
-
-<!--
-    Sections can be include here using section/stop format.
-
-    Supported sections are:
-
-        meta.opensearch
-        meta.title
-        meta.keywords
-        meta.description')  
-        meta.og
-        meta.files
-
-        sidebar.series
-        sidebar.popular
-
-        component.search
-        component.related
-
-        footer-nav.left
-        footer-nav.right
-
-        post.list
-        post.head
-        post.body
-
-        page.body
-
-        layout.view
-        layout.en
--->
