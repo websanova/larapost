f3ec6c505c06ce84a8bef9f1274bda4bb3f87315
websanova <rob@websanova.com>
2016-01-26 21:10:31 +0700

    Update sidebar and series bulleting.

--- resources/views/themes/default/layout.blade.php
+++ resources/views/themes/default/layout.blade.php
@@ -95,37 +95,37 @@
                     @include (lb_view('.components.search'))
                 </div>
 
-                <hr/>
-
                 @section ('sidebar.series')
                     @if ( ! $series->isEmpty())
-                        <h4>Series</h4>
+                        <h4 class="page-header">Series</h4>
 
-                        <div clas="row">
-                            <div class="col-xs-12">
-                                <ul class="list-group text-muted">
-                                    @foreach ($series as $s)
-                                        <li><a href="{{ $s->url }}">{{ $s->title }} ({{ $s->posts_count}} Parts)</a></li>
-                                    @endforeach
-                                </ul>
+                        @foreach ($series as $s)
+                            <div class="media">
+                                <div class="media-left media-middle">
+                                    <div class="bg-info">&nbsp;</div>
+                                </div>
+                                <div class="media-body">
+                                    <a href="{{ $s->url }}">{{ $s->title }} ({{ $s->posts_count}} Parts)</a></li>
+                                </div>
                             </div>
-                        </div>
+                        @endforeach
                     @endif
                 @show
 
                 @section ('sidebar.popular')
                     @if ( ! $top->isEmpty())
-                        <h4>Popular</h4>
+                        <h4 class="page-header">Popular</h4>
 
-                        <div clas="row">
-                            <div class="col-xs-12">
-                                <ul class="list-group text-muted">
-                                    @foreach ($top as $t)
-                                        <li><a href="{{ $t->url }}">{{ $t->full_title }}</a></li>
-                                    @endforeach
-                                </ul>
+                        @foreach ($top as $t)
+                            <div class="media">
+                                <div class="media-left media-middle">
+                                    <div class="bg-info">&nbsp;</div>
+                                </div>
+                                <div class="media-body">
+                                    <a href="{{ $t->url }}">{{ $t->full_title }}</a>
+                                </div>
                             </div>
-                        </div>
+                        @endforeach
                     @endif
                 @show
             </div>
--- resources/views/themes/default/post/show.blade.php
+++ resources/views/themes/default/post/show.blade.php
@@ -2,7 +2,7 @@
     <h1><a href="{{ $post->url }}">{{ $post->full_title }}</a></h1>
     <div class="text-muted">{{ $post->published_at->format('M d Y') }}</div>
 
-    <div>
+    <p>
     	@foreach ($post->tags as $t)
     		<a href="{{ $t->url }}" class="label label-info">{{ $t->name }}</a>
     	@endforeach
@@ -10,28 +10,31 @@
         @if ($post->serie)
             <a href="{{ $post->serie->url }}" class="label label-warning">{{ $post->serie->title }}</a>
         @endif
-    </div>
-
-    <br/>
+    </p>
 @show
 
 @section ('post.series')
     @if ($post->serie)
-        @foreach ($post->serie->posts as $p)
-            &nbsp;&nbsp;&nbsp;&bull;&nbsp;&nbsp;<a href="{{ $p->url }}">{{ $p->title }}</a><br/>
-        @endforeach
-        
-        <br/>
+        <p class="lead">
+            <ul>
+                @foreach ($post->serie->posts as $p)
+                    <li><a href="{{ $p->url }}">{{ $p->title }}</a></li>
+                @endforeach
+            </ul>
+        </p>
     @endif
 @show
 
 @section ('post.buttons')
     @if (@$post && @$post->meta->buttons)
-        @foreach ($post->meta->buttons as $k => $v)
-            <a href="{{ $v }}" class="btn btn-primary">{{ $k }}</a>
-        @endforeach
+        <p class="lead"></p>
 
-        <br/>
+        <div>
+            @foreach ($post->meta->buttons as $k => $v)
+                <a href="{{ $v }}" class="btn btn-primary">{{ $k }}</a>
+            @endforeach
+        </div>
+        
     @endif
 @show
 
