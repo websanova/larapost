2aa802de09fdfa137680ca3bf13b2832cb01736d
websanova <rob@websanova.com>
2016-01-27 11:32:33 +0700

    Fix but when no meta is set on buttons.

--- resources/views/themes/default/components/buttons.blade.php
+++ resources/views/themes/default/components/buttons.blade.php
@@ -1,7 +1,7 @@
 @section ('component.buttons')
     @if (@$post && @$post->buttons)
         <div>
-            @foreach ($post->meta->buttons as $v)
+            @foreach ($post->buttons as $v)
                 <a href="{{ @$v->link }}" class="btn btn-primary">
                     @if (@$v->icon)
                         <li class="fa fa-{{ $v->icon }}"></li>
