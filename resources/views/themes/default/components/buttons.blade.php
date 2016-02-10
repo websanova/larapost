31ee808935ea004a07cfe181925ec05c7318ad03
websanova <rob@websanova.com>
2016-02-10 18:08:41 +0700

    Change button sizes to small.

--- resources/views/themes/default/components/buttons.blade.php
+++ resources/views/themes/default/components/buttons.blade.php
@@ -2,7 +2,7 @@
     @if (@$post && @$post->buttons)
         <div>
             @foreach ($post->buttons as $v)
-                <a href="{{ @$v->link }}" class="btn btn-primary">
+                <a href="{{ @$v->link }}" class="btn btn-sm btn-primary">
                     @if (@$v->icon)
                         <li class="fa fa-{{ $v->icon }}"></li>
                     @endif
