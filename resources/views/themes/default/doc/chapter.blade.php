bc3b0ed5eec237e3362a0d118bb5c6d657a05793
websanova <rob@websanova.com>
2018-12-15 20:25:12 +0700

    Update chapters layout.

--- resources/views/themes/default/doc/chapter.blade.php
+++ resources/views/themes/default/doc/chapter.blade.php
@@ -1,5 +1,13 @@
-<h1 class="first">
-    {{ $post->title }}
-</h1>
+@section ('post.head')
+    <h1 class="first">
+        {{ $post->title }}
+    </h1>
+@show
 
-{!! $post->body !!}
+@section ('post.body')
+    <div>{!! $post->body !!}</div>
+@show
+
+@section ('post.related')
+    @include (larablog_view('components.related'))
+@show
